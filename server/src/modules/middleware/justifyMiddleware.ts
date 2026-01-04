f5edfc0d57acb62d3b339033b2fec82f95af4efe
Daniel Valino Ovalle <valinoovalledanieljesus@gmail.com>
2026-01-04 21:33:51 +0100

    [ADD] connect token logic to justify endpoint

--- server/src/modules/middleware/justifyMiddleware.ts
+++ server/src/modules/middleware/justifyMiddleware.ts
@@ -1,4 +1,51 @@
 import express from 'express'
+import { Request, Response, NextFunction } from "express";
+import { verifyJwt } from "../services/auth/token.js";
+import { consumeWords } from '../services/auth/rateLimit.js';
 
 
 export const textPlainMiddleware = express.text({ type: 'text/plain' });
+
+
+export function tokenMiddleware(req: Request, res: Response, next: NextFunction) {
+	const header = req.header("Authorization");
+	if (!header?.startsWith("Bearer ")) {
+		return res.status(401).json({ error: "Missing token" });
+	}
+
+	const token = header.slice(7);
+
+	try {
+		//	Throws exception if the token is not valid
+		const { email } = verifyJwt(token);
+
+		req.user = { email };
+		next();
+	} catch {
+		res.status(401).json({ error: "Invalid token" });
+	}
+}
+
+
+export function wordLimitMiddleware(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	
+	const email = req.user!.email;
+	const text = req.body as string;
+
+	if (!email) {
+		return res.status(401).send("Not authorized");
+	}
+
+	//Check for daily word limit
+	if (!consumeWords(email, text)) {
+		return res.status(402).json({
+			error: "Payment Required",
+		});
+	}
+
+	next();
+}
--- server/src/modules/routes.ts
+++ server/src/modules/routes.ts
@@ -1,5 +1,5 @@
 import { Router} from 'express';
-import { textPlainMiddleware } from './middleware/justifyMiddleware.js';
+import { textPlainMiddleware, tokenMiddleware, wordLimitMiddleware } from './middleware/justifyMiddleware.js';
 import { justifyController } from './controller/justifyController.js';
 import { jsonMiddleware } from './middleware/authMiddleware.js';
 import { authController } from './controller/authController.js';
@@ -8,7 +8,7 @@ export function apiRoutes() {
 	const router = Router();
 	
 	//Justify endpoint
-	router.post( '/justify', textPlainMiddleware, justifyController);
+	router.post( '/justify', textPlainMiddleware, tokenMiddleware, wordLimitMiddleware, justifyController);
 
 	//Router endpoint
 	router.post('/token', jsonMiddleware, authController);
