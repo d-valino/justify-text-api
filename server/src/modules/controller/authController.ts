f1349a91e02401457cefc87806bafe6bd4fdb0ae
Daniel Valino Ovalle <valinoovalledanieljesus@gmail.com>
2026-01-04 21:20:33 +0100

    [ADD] token generation endpoint

---
+++ server/src/modules/controller/authController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { signJwt } from "../services/auth/token.js";
+
+
+function isValidEmail(email: string): boolean {
+	//Regex verification to valid email formats
+	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+export function authController(req: Request, res: Response) {
+	const { email } = req.body;
+
+	if (typeof email !== "string" || !isValidEmail(email)) {
+		return res.status(400).json({ error: "Invalid email" });
+	}
+
+	// Create token with JWT
+	const token = signJwt({ email });
+
+	res.json({ token });
+}
--- server/src/modules/routes.ts
+++ server/src/modules/routes.ts
@@ -1,12 +1,17 @@
 import { Router} from 'express';
 import { textPlainMiddleware } from './middleware/justifyMiddleware.js';
 import { justifyController } from './controller/justifyController.js';
+import { jsonMiddleware } from './middleware/authMiddleware.js';
+import { authController } from './controller/authController.js';
 
 export function apiRoutes() {
 	const router = Router();
 	
 	//Justify endpoint
 	router.post( '/justify', textPlainMiddleware, justifyController);
+
+	//Router endpoint
+	router.post('/token', jsonMiddleware, authController);
 
 	return router;
 }
