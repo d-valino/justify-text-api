29fc447cd513514f1e80dad84877dfedbe80bbf4
Daniel Valino Ovalle <valinoovalledanieljesus@gmail.com>
2026-01-05 03:05:02 +0100

    [FIX] improve text justification logic

--- server/src/modules/services/justify/justify.ts
+++ server/src/modules/services/justify/justify.ts
@@ -1,48 +1,102 @@
 const LINE_LIMIT = 80;
 
-export default function justify(input: string): string {
-	const words = input.split(/\s+/);
-	let output = '';
-	
-	let i = 0;
+
+function fillLine(state: {index: number}, words: string[], line: string[]): { totalLetters: number; endedBySplit: boolean }
+{
 	const number_of_words = words.length;
-
-	while (i < number_of_words) {
-		const line = [];
-		let total_letters = 0;
-
-		// Select words
-		while (i < number_of_words && total_letters + words[i].length + line.length <= LINE_LIMIT) {
-			total_letters += words[i].length;
-			line.push(words[i]);
-			i++;
+	let totalLetters = 0;
+	let endedBySplit = false;
+	while (state.index < number_of_words)
+	{
+		// line.length accounts for spaces between words
+		if (totalLetters + words[state.index].length + line.length <= LINE_LIMIT)
+		{
+			totalLetters += words[state.index].length;
+			line.push(words[state.index]);
+			state.index++;
+			continue;
 		}
+		else if (words[state.index].length + totalLetters > LINE_LIMIT)
+		{
+			const spaceLeft = LINE_LIMIT - (totalLetters + line.length);
+			if (spaceLeft > 0)
+			{
+				const part1 = words[state.index].substring(0, spaceLeft);
+				const part2 = words[state.index].substring(spaceLeft);
+				line.push(part1);
+				totalLetters += part1.length;
+				words[state.index] = part2;
+				endedBySplit = true;
+			}
+		}
+		break ;
+	}
+	return {totalLetters, endedBySplit};
+}
 
-		if (line.length === 1) {
-			output += line[i] + '\n';
+
+function justifyLine(line: string[], total_letters: number): string
+{
+	if (line.length <= 1)
+		return line[0] + '\n';
+
+	const total_spaces = LINE_LIMIT - total_letters;
+	const normal_spaces = Math.floor(total_spaces / (line.length - 1));
+	let extra_spaces = total_spaces % (line.length - 1);
+	
+	let justified_line = '';
+	for (let index = 0; index < line.length; index++)
+	{
+		justified_line += line[index];
+		if (index < line.length - 1)
+		{
+			justified_line += ' '.repeat(normal_spaces);
+			if (extra_spaces > 0)
+			{
+				justified_line += ' ';
+				extra_spaces--;
+			}
+		}
+	}
+	return (justified_line + '\n');
+}
+
+
+// Justify only one paragraph
+function justifyParagraph(paragraph: string): string
+{
+	const clean_paragraph = paragraph.replace(/\n/g, ' ').trim();
+	if (!clean_paragraph)
+		return ('');
+
+	const words = clean_paragraph.split(/\s+/);
+	let output = '';
+
+	const state = {index: 0};
+	const number_of_words = words.length;
+	while (state.index < number_of_words)
+	{
+		const line: string[] = [];
+		
+		// Select words to fill the line
+		const { totalLetters, endedBySplit } = fillLine(state, words, line);
+
+		// No justification if the line is 1 word long or is the last line
+		if (line.length === 1 || (state.index === number_of_words && !endedBySplit)) {
+			output += line.join(' ') + '\n';
 			continue;
 		}
 
-		// Calculate number of spaces
-		const total_spaces = LINE_LIMIT - total_letters;
-		const normal_spaces = Math.floor(total_spaces / (line.length - 1));
-		let extra_spaces = total_spaces % (line.length - 1);
-
-		// Justify line
-		let justified_line = '';
-		for (let index = 0; index < line.length; index++) {
-			justified_line += line[index];
-			if (index < line.length - 1) {
-				justified_line += ' '.repeat(normal_spaces);
-				if (extra_spaces > 0) {
-					justified_line += ' ';
-					extra_spaces--;
-				}
-			}
-		}
-		justified_line += '\n';
-		output += justified_line;
+		output += justifyLine(line, totalLetters);
 	}
+	return (output.trimEnd() + '\n');
+}
 
-	return output;
+
+export default function justify(input: string): string
+{
+	const paragraphs = input.split(/\n\s*\n/);
+
+	// Process each paragraph and join
+	return (paragraphs.map(p => justifyParagraph(p)).join(''));
 }
